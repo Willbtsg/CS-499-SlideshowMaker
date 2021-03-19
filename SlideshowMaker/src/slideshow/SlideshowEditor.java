9f8170de8099ce4bdf4006addea1019a70960ce7
Will Thomason <willbtsg@gmail.com>
2021-03-19 10:40:49 -0500

    Added GUI Transition retrieval to TransitionLibrary.

--- SlideshowMaker/src/slideshow/SlideshowEditor.java
+++ SlideshowMaker/src/slideshow/SlideshowEditor.java
@@ -1,16 +1,38 @@
 package slideshow;
 
+import transitions.Transition;
+import transitions.TransitionLibrary;
+
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
+
+/**
+ * Class name: SlideshowEditor
+ *
+ * This class manages the GUI components necessary for creating and exporting a Slideshow.
+ */
 
 public class SlideshowEditor extends JFrame {
 
+    /**
+     * SlideshowEditor instance- instance of SlideshowEditor used for Singleton implementation
+     * JPanel m_ImageLibrary- contains the ImageLibrary GUI component
+     * JPanel m_AudioLibrary- contains the AudioLibrary GUI component
+     * TransitionLibrary m_TransitionLibrary- contains reference to TransitionLibrary for adding Transition to Slides
+     * JPanel m_controlPanel- contains the buttons used to swap between active libraries
+     * JButton getImages- loads the ImageLibrary
+     * JButton getAudio- loads the AudioLibrary
+     * JButton addTransitions- used to add Transitions to the selected Slide in Timeline
+     */
     private static SlideshowEditor instance;
     private static JPanel m_ImageLibrary;
     private static JPanel m_AudioLibrary;
+    private TransitionLibrary m_TransitionLibrary;
     private JPanel m_controlPanel;
     private JButton getImages;
     private JButton getAudio;
+    private JButton addTransitions;
 
     public static void main(String[] args)
     {
@@ -32,6 +54,8 @@ public class SlideshowEditor extends JFrame {
         add(m_ImageLibrary);
          */
 
+        m_TransitionLibrary = TransitionLibrary.getInstance();
+
         m_controlPanel = new JPanel();
         m_controlPanel.setLayout(null);
         m_controlPanel.setBackground(Color.LIGHT_GRAY);
@@ -45,10 +69,17 @@ public class SlideshowEditor extends JFrame {
         getImages.addActionListener(event -> showImageLibrary());
 
         getAudio = new JButton("Audio Library");
-        getAudio.setBounds(1200, 20, 120, 20);
+        getAudio.setBounds(300, 20, 120, 20);
         m_controlPanel.add(getAudio);
 
         getAudio.addActionListener(event -> showAudioLibrary());
+
+        addTransitions = new JButton("Add Transitions");
+        addTransitions.setBounds(1200, 20, 150, 20);
+        m_controlPanel.add(addTransitions);
+
+        //this function will eventually pull the selected Slide form timeline to use as an argument
+        //addTransitions.addActionListener(event -> addSlideTransitions(testSlide));
 
         add(m_controlPanel);
 
@@ -56,22 +87,44 @@ public class SlideshowEditor extends JFrame {
         setSize(1400,800); //1400 width and 800 height
         setLocationRelativeTo(null);
         setVisible(true); //making the frame visible
-        setResizable(false); //disable maximize button
 
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+
     }
 
+    /**
+     * This function is used to make m_ImageLibrary the active JPanel
+     */
     private void showImageLibrary()
     {
         remove(m_AudioLibrary);
         add(m_ImageLibrary);
     }
 
+    /**
+     * This function is used to make m_AudioLibrary the active JPanel
+     */
     private void showAudioLibrary()
     {
         remove(m_ImageLibrary);
         add(m_AudioLibrary);
-        System.out.println("ooga");
+    }
+
+    /**
+     * This button is used when adding Transitions to the Slide selected in Timeline
+     * @param selectedSlide- Slide from Timeline the user wishes to add Transitions to
+     */
+    private void addSlideTransitions(Slide selectedSlide)
+    {
+        ArrayList<Transition> desiredTransitions = new ArrayList<Transition>();
+
+        desiredTransitions = m_TransitionLibrary.retrievalGUI(); //use TransitionLibrary GUI-based retrieval function
+
+        if (desiredTransitions.size() > 0) //if user didn't decide to stop adding the Transition...
+        {
+            selectedSlide.setForward(desiredTransitions.get(0)); //...set the forwards and backwards Transitions
+            selectedSlide.setBackwards(desiredTransitions.get(1));
+        }
     }
 
     /**
--- SlideshowMaker/src/transitions/TransitionLibrary.java
+++ SlideshowMaker/src/transitions/TransitionLibrary.java
@@ -1,4 +1,5 @@
 package transitions;
+import javax.swing.*;
 import java.util.ArrayList;
 
 /**
@@ -12,20 +13,6 @@ import java.util.ArrayList;
 public class TransitionLibrary {
 
     private static TransitionLibrary instance;
-
-    /**
-     * Returns instance of TransitionLibrary. If no instance exists, one is created.
-     * @return instance- reference to the TransitionLibrary Singleton
-     */
-    public static TransitionLibrary getInstance() {
-
-        if (instance == null)
-        {
-            instance = new TransitionLibrary();
-        }
-
-        return instance;
-    }
 
     /**
      * Supplies calling object with the appropriate pair of Transition objects
@@ -66,4 +53,63 @@ public class TransitionLibrary {
 
         return desiredTransitions;
     }
+
+    /**
+     * This function is used when the user adds Transitions to Slides in the SlideshowEditor
+     * @return
+     */
+    public ArrayList<Transition> retrievalGUI()
+    {
+        ArrayList<Transition> desiredTransitions = new ArrayList<Transition>();
+
+        String transitions[] = {"LRWipe", "RLWipe", "UpWipe", "DownWipe", "CrossFade"}; //set list of Transition options
+
+        String transChoice;
+        transChoice = (String) JOptionPane.showInputDialog(null, "What transition would you like to use?", "Transition Select", JOptionPane.PLAIN_MESSAGE, null, transitions, transitions[0]);
+
+        if (transChoice != null) //if user made a selection, proceed with Transition creation
+        {
+            desiredTransitions = retrieveTransitions(transChoice); //get Transition from library
+
+            if (desiredTransitions.size() > 0) //if Transition checkout was successful, proceed
+            {
+                Double times[] = {1.0, 2.0, 3.0, 4.0, 5.0}; //set options for Transition length
+
+                Double timeChoice;
+                timeChoice = (Double) JOptionPane.showInputDialog(null, "How many seconds do you want your transition to last?", "Transition Select", JOptionPane.PLAIN_MESSAGE, null, times, times[0]);
+
+                if (timeChoice != null) //if user made a valid Transition length selection...
+                {
+
+                    long timeLong = timeChoice.longValue() * 1000; //...convert Double second time to long millisecond time...
+
+                    for (Transition transition : desiredTransitions) //...and set that as the Transition length
+                    {
+                        transition.setTime(timeLong);
+                    }
+
+                } else { //if a valid length was not selected...
+                    desiredTransitions.clear(); //clear the list of Transitions (i.e. don't add any to the Slide)
+                }
+            }
+        }
+
+        return desiredTransitions;
+
+    }
+
+    /**
+     * Returns instance of TransitionLibrary. If no instance exists, one is created.
+     * @return instance- reference to the TransitionLibrary Singleton
+     */
+    public static TransitionLibrary getInstance() {
+
+        if (instance == null)
+        {
+            instance = new TransitionLibrary();
+        }
+
+        return instance;
+    }
+
 }
